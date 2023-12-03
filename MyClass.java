bc24533f5cea33ee1c0f6787bfb63f78a698d28e
Sara Usevska <sarausevska42@yahoo.com>
2023-12-03 17:51:23 +0100

    first commit

---
+++ MyClass.java
@@ -0,0 +1,17 @@
+package project_28;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		Vehicle v1 = new Vehicle();
+		v1.setColor("Red");
+		System.out.println(v1.getColor());
+        v1.setX(10);
+        System.out.println(v1.getX());
+        v1.setB("New");
+        System.out.println(v1.getB());
+        v1.setY(9);
+        System.out.println(v1.getY());
+	}
+
+}
---
+++ Vehicle.java
@@ -0,0 +1,40 @@
+package project_28;
+
+public class Vehicle {
+	private String color;
+	private int x;
+	private String b;
+	private int y;
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public String getB() {
+		return b;
+	}
+
+	public void setB(String b) {
+		this.b = b;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+	}
